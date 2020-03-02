19b5534ea6b70ca2d0c68e8dcaa53785358ccf28
Joe Salazar <jcsalazar77@gmail.com>
2020-03-02 14:03:31 -0500

    added challenge lab

---
+++ lab_input/challenge01.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+user_input = input("What is your name?:")
+user_input2 = input("What day of the week is it?:")
+#print("Hello" + "user_input" + ",", "happy" + "user_input2" + "!")
+print(f"Hello, {user_input}! Happy {user_input2}!")
