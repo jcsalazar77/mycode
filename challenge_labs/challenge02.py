5a27801139df73634ed0a81481aba43882f9156e
Joe Salazar <jcsalazar77@gmail.com>
2020-03-02 15:49:00 -0500

    Adding challenge labs 1 and 2

---
+++ challenge_labs/challenge02.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+c_list = ["blue", "Columbus"]
+name = input("What is your name?:")
+c_list.append(1492)
+print(f"In {c_list[2]},  {c_list[1]} sailed the ocean {c_list[0]}. {name} fell off the boat.")
