1db7dda130f6b648206ea995b6823a85ef2ddc9f
Joe Salazar <jcsalazar77@gmail.com>
2020-03-04 10:27:54 -0500

    added lab 22

---
+++ introfor/forloop1.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+#create list called vendors
+vendors = ["cisco", "juniper", "big ip", "f5", "arista"]
+
+#loop across the list of vendors
+for x in vendors:
+    print("The vendor is: " + x) #each time through the loop print the value of x
+
+print("\nOur loop has ended.") #when the loop ends, print this
