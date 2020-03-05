dfa00116d70d23748b607d1c5972b065afdb87d0
Joe Salazar <jcsalazar77@gmail.com>
2020-03-05 14:02:56 -0500

    added lab 35 OS.WALK

---
+++ lrnimport/intscript.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+from subprocess import call
+call(["ip", "link", "show", "up"])
+print('This program will check your interfaces.')
+
+interface = input('Enter and interface, like, ens3: ')
+
+call(["ip", "addr", "show", "dev", interface])
+call(["ip", "route", "show", "dev", interface])
+
